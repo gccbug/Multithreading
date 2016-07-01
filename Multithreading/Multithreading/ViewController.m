31000a1f01cfbbab5eb8dd6d22e01a0c9c1cb882
zhengbing <83852084@qq.com>
2016-07-01 15:20:55 +0800

    add some changes

--- Multithreading/Multithreading/ViewController.m
+++ Multithreading/Multithreading/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "SubViewController.h"
 
 #define CELLID @"ViewController"
 
@@ -32,9 +31,41 @@
 
 #pragma mark UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    SubViewController *sub = [[SubViewController alloc] init];
-    sub.tag = indexPath.row;
-    [self.navigationController pushViewController:sub animated:YES];
+    switch (indexPath.row) {
+        case 0:
+        {
+            [self method1];
+        }
+            break;
+        case 1:
+        {
+            [self method2];
+        }
+            break;
+        case 2:
+        {
+            [self method3];
+        }
+            break;
+        case 3:
+        {
+            [self method4];
+        }
+            break;
+        case 4:
+        {
+            [self method5];
+        }
+            break;
+        case 5:
+        {
+            [self method6];
+        }
+            break;
+            
+        default:
+            break;
+    }
 }
 
 #pragma mark UITableViewDataSource
@@ -66,9 +97,161 @@
 
 -(NSArray *)tableViewDataSource{
     if (!_tableViewDataSource) {
-        _tableViewDataSource = @[@"1",@"2",@"3",@"4"];
+        _tableViewDataSource = @[@"1 - dispatch_queue_t1",
+                                 @"2 - dispatch_queue_t2",
+                                 @"3 - dispatch_queue_t3",
+                                 @"4 - NSBlockOperation start使用方法",
+                                 @"5 - NSOperationQueue1",
+                                 @"6 - NSOperationQueue2"];
     }
     return _tableViewDataSource;
 }
+
+#pragma mark test method
+
+- (void)method1{
+    /**  优先级
+     *  #define DISPATCH_QUEUE_PRIORITY_HIGH 2      优先
+     *  #define DISPATCH_QUEUE_PRIORITY_DEFAULT 0   正常
+     *  #define DISPATCH_QUEUE_PRIORITY_LOW (-2)    低
+     *  #define DISPATCH_QUEUE_PRIORITY_BACKGROUND INT16_MIN 后台
+     */
+    //创建异步队列
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    //往队列里添加线程操作
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片1--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片2--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片3--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片4--%@",[NSThread currentThread]);
+    });
+}
+
+- (void)method2{
+    /**
+     *  创建串行队列
+     *  @param "Myqueue.test" 标识符
+     *  @param NULL           一般为NULL
+     */
+    dispatch_queue_t queue = dispatch_queue_create("Myqueue.test", NULL);
+    //开启异步线程
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片1--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片2--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片3--%@",[NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"---下载图片4--%@",[NSThread currentThread]);
+    });
+}
+
+- (void)method3{
+    //设置异步队列
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    //设置组队列
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"---下载图片1--%@",[NSThread currentThread]);
+    });
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"---下载图片2--%@",[NSThread currentThread]);
+    });
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"---下载图片3--%@",[NSThread currentThread]);
+    });
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"---下载图片4--%@",[NSThread currentThread]);
+    });
+    //组队列操作执行完成后调用
+    dispatch_group_notify(group, queue, ^{
+        NSLog(@"下载完成");
+    });
+}
+
+- (void)method4{
+    //NSBlockOperation start使用方法
+    dispatch_async(dispatch_queue_create("123", NULL), ^{
+        NSBlockOperation *operation1 = [NSBlockOperation blockOperationWithBlock:^{
+            NSLog(@"---下载图片1--%@",[NSThread currentThread]);
+        }];
+        //增加额外线程 addExecutionBlock 额外线程是在operation1主线程执行后 再异步执行
+        [operation1 addExecutionBlock:^{
+            NSLog(@"---下载图片2--%@",[NSThread currentThread]);
+        }];
+        [operation1 addExecutionBlock:^{
+            NSLog(@"---下载图片3--%@",[NSThread currentThread]);
+        }];
+        [operation1 addExecutionBlock:^{
+            NSLog(@"---下载图片4--%@",[NSThread currentThread]);
+        }];
+        //用start 直接使用当前线程
+        [operation1 start];
+    });
+}
+
+- (void)method5{
+    //创建异步队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    //创建主线程队列
+    //    NSOperationQueue *queue = [NSOperationQueue mainQueue];
+    //若为主线程 则相当于串行队列
+    [queue addOperationWithBlock:^{
+        NSLog(@"---下载图片1--%@",[NSThread currentThread]);
+    }];
+    [queue addOperationWithBlock:^{
+        NSLog(@"---下载图片2--%@",[NSThread currentThread]);
+    }];
+    //创建操作
+    NSBlockOperation *operation1 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"---下载图片1--%@",[NSThread currentThread]);
+    }];
+    NSBlockOperation *operation2 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"---下载图片2--%@",[NSThread currentThread]);
+    }];
+    NSBlockOperation *operation3 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"---下载图片3--%@",[NSThread currentThread]);
+    }];
+    NSBlockOperation *operation4 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"---下载图片4--%@",[NSThread currentThread]);
+    }];
+    //将操作添加进队列
+    [queue addOperation:operation1];
+    [queue addOperation:operation2];
+    [queue addOperation:operation3];
+    [queue addOperation:operation4];
+}
+
+- (void)method6{
+    //创建异步队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    //创建操作
+    NSBlockOperation *operationA = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"-----A------");
+    }];
+    NSBlockOperation *operationB = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"-----B------");
+    }];
+    NSBlockOperation *operationC = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"-----C------");
+    }];
+    //设置依赖关系 不能相互依赖
+    [operationB addDependency:operationA];
+    [operationC addDependency:operationA];
+    //将操作写进队列
+    [queue addOperation:operationA];
+    [queue addOperation:operationB];
+    [queue addOperation:operationC];
+}
+
 
 @end
